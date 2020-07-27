fc92af203d6c75a620b752831756c5dbc5d934d2
Tommaso Venza <tommasovenza@gmail.com>
2020-07-27 16:05:13 +0200

    creazione del database

---
+++ database.php
@@ -0,0 +1,47 @@
+<?php 
+    $servername = 'localhost';
+    $username = 'root';
+    $password = 'root';
+    $dbname = 'db_hotel';
+
+    // connect
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // check connection
+    if($conn && $conn->connect_error) {
+        $database = [
+            'error' => $conn->connect_error
+        ];
+    } else {
+        // ESECUZIONE PROGRAMMA
+        $sql = "SELECT * FROM `stanze`"; 
+        $results = $conn->query($sql);
+
+        $database = [];
+
+        // se ho risultati dalla query
+        if($results && $results->num_rows > 0) { 
+
+            
+            // per ogni riga faccio qualcosa...
+            while($row = $results->fetch_assoc()) {
+                $database[] = $row;
+            }
+        } elseif ($results) {
+            $database = [];
+        } else {
+            $database = [
+                'error' => 'query error, errore di sintassi'
+            ];
+
+        }
+        // FINE ESECUZIONE PROGRAMMA
+    } 
+
+
+
+
+
+
+?>
+
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include __DIR__ . '/database.php'; ?>
+<?php var_dump($database); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+</body>
+</html>
