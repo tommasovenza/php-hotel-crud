b442177c9375a2f3151bf199a08f16ed85207ed1
Tommaso Venza <tommasovenza@gmail.com>
2020-07-27 16:21:57 +0200

    prima stampa di prova

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php include __DIR__ . '/database.php'; ?>
-<?php var_dump($database); ?>
+<?php // var_dump($database); ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>DB test</title>
 </head>
 <body>
+
+  <?php if ( !isset( $database['error'] ) ) { ?>
+
+    <ul>
+        <?php foreach ($database as $stanza) { ?>
+            
+            <li> 
+                <div>ID: <?php echo $stanza['id']; ?></div>
+                <div>Room Number: <?php echo $stanza['room_number']; ?></div>   
+                <div>Floor: <?php echo $stanza['floor']; ?></div>
+                <div>Beds: <?php echo $stanza['beds']; ?></div>
+            </li>
+
+        <?php } ?>
+    </ul>
+
+  <?php } ?>
     
 </body>
 </html>
